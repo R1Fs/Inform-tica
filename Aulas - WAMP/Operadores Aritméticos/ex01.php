8c92ec232fb9ce2e08e619c464d05cf5976a61a3
RicardoFs <63893693+R1Fs@users.noreply.github.com>
2022-03-10 21:19:22 -0300

    Add files via upload

---
+++ Aulas - WAMP/Operadores Aritméticos/ex01.php
@@ -0,0 +1,19 @@
+<?php
+echo "<meta charset = 'utf-8'>";
+echo "Operadores Aritméticos<br><br>";
+
+$x = 8;
+$y = 2; 
+
+
+$soma = $x + $y; 
+$sub = $x - $y;
+$mult = $x * $y;
+$div = $x / $y;
+
+echo "$x + $y; = $soma<br>";
+echo "$x - $y; = $sub<br>";
+echo "$x * $y; = $mult<br>";
+echo "$x / $y; = $div<br>";
+
+?>
---
+++ Aulas - WAMP/Variáveis e Mostrar na Tela/ex01.php
@@ -0,0 +1,9 @@
+<?php
+echo "<meta charset = 'UTF-8'>";
+echo "Primeira Aplicação<br><br>";
+
+$nome = "Ricardo";
+$idade = 15;
+$media = 7;
+
+echo "$nome tem $idade e a média é $media";

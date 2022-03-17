3e1970641b1686ff85ac17d24e4a422f8660a02e
RicardoFs <63893693+R1Fs@users.noreply.github.com>
2022-03-17 17:38:25 -0300

    Add files via upload

---
+++ Aulas - WAMP/Estrutura de Decisão Simples/ex01.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Estrutura de Decisão Simples<br><br>";
+
+$x = $_POST['primeiro'];
+$y = $_POST['segundo'];
+
+if ($x > $y) {
+
+	echo "$x é maior que $y";
+
+}else{
+
+	echo "$x é menor que $y";
+
+
+
+}
+
+?>

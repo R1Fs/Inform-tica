3615c58280999c1671abecb1236fa840db29e14e
RicardoFs <63893693+R1Fs@users.noreply.github.com>
2022-04-11 16:51:48 -0300

    Add files via upload

---
+++ Linguagem C/Entrada mais de um número/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float n1,n2,n3,n4,media;
+	
+	printf("Digite 4 números: ");
+	scanf("%f,%f,%f,%f",&n1,&n2,&n3,&n4);
+	
+	media = (n1+n2+n3+n4)/4;
+	
+	printf("A média é: %.2f\n",media);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}

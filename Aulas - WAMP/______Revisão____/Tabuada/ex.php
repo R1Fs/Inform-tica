536588f91a92f8a2f9b18f47bc53515c05c73a38
RicardoFs <63893693+R1Fs@users.noreply.github.com>
2022-05-19 19:51:21 -0300

    Add files via upload

---
+++ Aulas - WAMP/______Revisão____/Tabuada/ex.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+echo "Tabuada <br><Br>";
+
+$numero = $_GET['numero'];
+
+
+for ($i=0; $i <=10 ; $i++) { 
+	
+
+echo $numero. ' X ' .$i. ' = ' .$numero*$i. '<br>';
+
+
+
+
+}
+
+
+
+
+
+
+
+ ?>
